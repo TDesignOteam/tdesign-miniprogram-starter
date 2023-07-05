9ba7639a58486d17344c22f1a51a77e2b6cd7aaf
leejimchill <273743552@qq.com>
2023-07-05 11:37:46 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+App({
+  onLaunch() {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
