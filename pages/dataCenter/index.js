dd74944b17436e1064ef5f9a85b2c7b9e6e01f76
随便 <oncwnuB14xVJ24ubJmmzL9rKAQ0o@git.weixin.qq.com>
2023-08-21 20:59:14 +0800

    v1.0

--- pages/dataCenter/index.js
+++ pages/dataCenter/index.js
@@ -6,8 +6,13 @@ Page({
    */
   data: {
     total_situation_dataList: null,
+    total_situation_keyList: null,
     complete_rate_dataList: null,
+    complete_rate_keyList: null,
     interaction_situation_dataList: null,
+    interaction_situation_keyList: null,
+    area_dataList: null,
+    area_data_keysList : null,
     itemWidth: null,
     itemWidth: null,
     smallitemWidth: null
@@ -19,13 +24,18 @@ Page({
   onLoad(options) {
     console.log("hello world!")
     var that = this;
+    
+    /**
+     * 整体情况
+     */
     wx.request({
       url: 'https://example.com/ajax?dataType=member',
       dataType: 'json',
       success(res) {
         var total_situation_data = res.data.list;
         that.setData({ 
-          total_situation_dataList: total_situation_data
+          total_situation_dataList: total_situation_data,
+          total_situation_keysList: Object.keys(total_situation_data[0])
         });
 
         // 计算每个.item元素的宽度
@@ -41,13 +51,17 @@ Page({
       }
     });
 
+    /**
+     * 互动情况
+     */
     wx.request({
       url: 'https://example.com/ajax?dataType=interaction',
       dataType: 'json',
       success(res) {
         var interaction_situation_data = res.data.list;
         that.setData({ 
-          interaction_situation_dataList: interaction_situation_data
+          interaction_situation_dataList: interaction_situation_data,
+          interaction_situation_keysList: Object.keys( interaction_situation_data[0])
         });
 
         // 计算每个.item元素的宽度
@@ -61,18 +75,21 @@ Page({
         console.log('smallitemWidth:', that.data.smallitemWidth);
       }
     });
-
+    /**
+     * 完播率
+     */ 
     wx.request({
       url: 'https://example.com/ajax?dataType=complete_rate',
       dataType: 'json',
       success(res) {
         var complete_rate_data = res.data.list;
         that.setData({ 
-          complete_rate_dataList: complete_rate_data
+          complete_rate_dataList: complete_rate_data,
+          complete_rate_keysList: Object.keys(complete_rate_data[0])
         });
 
         // 计算每个.item元素的宽度
-        var itemHeight = (344 - 16 * (complete_rate_data.length - 1)) / complete_rate_data.length + 'rpx';
+        var itemHeight = (380 - 16 * (complete_rate_data.length - 1)) / complete_rate_data.length + 'rpx';
 
         // 更新.item元素的样式
         that.setData({
@@ -84,6 +101,27 @@ Page({
       }
     });
     
+    /**
+     * 按区域统计
+     */
+    wx.request({
+      url: 'https://example.com/ajax?dataType=area',
+      dataType: 'json',
+      success(res) {
+        var area_data = res.data.list;
+        
+        that.setData({ 
+          area_dataList: area_data,
+          area_data_keysList: Object.keys(area_data[0])
+        });
+        
+        console.log('area_dataList:', that.data.area_dataList);
+        console.log('area_data_keysList:', that.data.area_data_keysList);
+      }
+    });
+
+    console.log("test" , this.data.test)
+    console.log("test keys ", Object.keys(this.data.test[0]))
   },
 
   /**
