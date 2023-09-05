dbff092a810efbea929e66901c5db99441809470
Aaliyha <623201572@qq.com>
2023-09-05 19:30:26 +0800

    解决合并远程仓库main分支冲突5

--- pages/home/index.js
+++ pages/home/index.js
@@ -10,9 +10,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData:
-      wx.canIUse('open-data.type.userAvatarUrl') &&
-      wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
   },
   // 事件处理函数
   bindViewTap() {
