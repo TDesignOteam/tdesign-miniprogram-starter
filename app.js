c190efc95eb0a262b8a363b420d66c9020151eeb
shinyina <shinyina@foxmail.com>
2023-09-06 11:23:18 +0800

    fix lint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import config from './config'
 import Mock from './mock/index'
 import createBus from './utils/eventBus'
 import { connectSocket, fetchUnreadNum } from './services/chat'
+
 if (config.isMock) {
   Mock()
 }
