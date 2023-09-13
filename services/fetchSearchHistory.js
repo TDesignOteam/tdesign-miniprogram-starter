f59d2f396697942fb18f1afcb753c3b9b00897f8
廖欣茵 <96294629+Sacvengers@users.noreply.github.com>
2023-09-13 10:27:57 +0800

    feat:完成【搜索】 (#20)
    * å®feat:完成【ã搜索】
    
    * fix:eslint报错修复
    
    * fix:修复问题
    
    ---------
    
    Co-authored-by: liaoxinyin <2496922253@qq.com>
    Co-authored-by: lijun <leejimqiu@gmail.com>

---
+++ services/fetchSearchHistory.js
@@ -0,0 +1,33 @@
+import { config } from '../config/index';
+import delay from './delay';
+import { getSearchHistoryMock, getSearchPopularMock } from '../model/search';
+
+/** 获取搜索历史 */
+function mockSearchHistory() {
+  return delay().then(() => getSearchHistoryMock());
+}
+
+/** 获取搜索历史 */
+export function getSearchHistory() {
+  if (config.useMock) {
+    return mockSearchHistory();
+  }
+  return new Promise((resolve) => {
+    resolve('real api');
+  });
+}
+
+/** 获取搜索热点 */
+function mockSearchPopular() {
+  return delay().then(() => getSearchPopularMock());
+}
+
+/** 获取搜索热点 */
+export function getSearchPopular() {
+  if (config.useMock) {
+    return mockSearchPopular();
+  }
+  return new Promise((resolve) => {
+    resolve('real api');
+  });
+}
